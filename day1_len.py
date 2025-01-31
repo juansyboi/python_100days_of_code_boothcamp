587558e2931745833df735f6da61efe97b4a7ff0
john o <john.o.work20@gmail.com>
2025-01-31 11:48:49 +0800

    added new project and quiz

---
+++ day1_len.py
@@ -0,0 +1,15 @@
+#direct focus
+
+name = input("What is your name? ")
+
+print("Hello " + name + "!")
+print("Number of letters in your name is ", len(name), "!!") #without using str first
+
+
+#variable focus
+
+name = input("What is your name? ")
+length_name = len(name)
+
+print("Hello " + name + "!")
+print("Number of letters in your name is ", length_name, "!!")
