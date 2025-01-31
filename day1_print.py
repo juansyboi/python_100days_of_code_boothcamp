f32c9e706d35d9465e57ab684a35f0683b3f4e1b
john o <john.o.work20@gmail.com>
2025-01-31 08:51:52 +0800

    updated day1_print

--- day1_print.py
+++ day1_print.py
@@ -1,2 +1,6 @@
 print("hello world") #print string
-print(123) #print value/number
+print(123) #print int
+print(1.3) #print float
+print("hello world #",2) #print string+int
+print("`Hello world`") #print with ``
+print("'hello world'") #print with '' single quotes
